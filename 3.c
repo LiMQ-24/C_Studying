c98ab792553a4420b19bb4d1bcd16fe05e47797c
LiMQ <92210631+LiMQ-24@users.noreply.github.com>
2021-11-16 19:48:03 +0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,11 @@
+/*
+编写一个程序
+打印你的姓名和地址
+*/
+#include<stdio.h>
+int main(int argc,char const*argv[])
+{
+    printf("Name:   Li mingqiang\n");
+    printf("Address:Lanzhou University Of Technology\n");
+return 0;   
+}
